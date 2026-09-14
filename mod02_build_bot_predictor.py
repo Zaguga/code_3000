c74b1ad1aff19f2dadb27fe68af130959e51934b
Bridget Dolan <bdolan264@gmail.com>
2026-09-14 16:34:48 -0400

    optimal rate .111

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -11,10 +11,12 @@ def train_model(X, y, seed=seed):
     """
     model = GradientBoostingClassifier(
         learning_rate=0.1,
-        n_estimators=100,
-        max_depth=5,
+        n_estimators=100, #from 100 to 150 -- Made it worse, reverted back to 100
+        max_depth=3, #from 8 to 4 -- to 3
         subsample=1,
-        min_samples_leaf=1,
+        min_samples_leaf=2, #from 1 to 2 --back to 1 -- increasing this lowered it by .002 -- testing to 4 now.. (keeping other 3)
+        # 4 raised it again, keep at 2, and decrease max dept to 2? -- Still not most optimal (.112)
+        #current is most optimal at .111
         random_state=seed
     )
     model.fit(X, y)
